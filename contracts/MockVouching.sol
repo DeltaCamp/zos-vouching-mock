e4867ca811841e5fd1bcfd2e96085118dac96d7c
Brendan Asselstine <mail.asselstine@gmail.com>
2019-01-23 10:23:46 -0800

    Added event testing functions to MockVouching

--- contracts/MockVouching.sol
+++ contracts/MockVouching.sol
@@ -3,4 +3,14 @@ pragma solidity ^0.4.24;
 import "zos-vouching/contracts/Vouching.sol";
 
 contract MockVouching is Vouching {
+  event Test(uint256 _amount);
+  event Test2(uint256 _amount);
+
+  function doTest(uint256 _amount) public {
+    emit Test(_amount);
+  }
+
+  function doTest2(uint256 _amount) public {
+    emit Test2(_amount);
+  }
 }
